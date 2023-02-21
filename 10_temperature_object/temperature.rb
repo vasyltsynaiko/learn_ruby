898078e0e8b58bcb90d82668ee663c0541634274
Vasyl <vasyl.tsynaiko@gmail.com>
2023-02-21 13:48:24 +0200

    done task 11(10_temp_object)

---
+++ 10_temperature_object/temperature.rb
@@ -0,0 +1,36 @@
+class Temperature
+    
+    def initialize(hash = {})  
+        @hash = hash   
+    end
+    def self.from_celsius(temp)
+        Temperature.new({c: temp})
+    end
+    
+    def self.from_fahrenheit(temp)
+        Temperature.new({f: temp})
+    end
+    
+    def in_fahrenheit
+        if @hash.has_key?(:f)
+            return @hash[:f] 
+        elsif @hash.has_key?(:c)
+            return @hash[:c]*9.to_f/5+32 
+        elsif @temp1.is_a? Numeric
+            return @temp1*9.to_f/5+32
+        else
+            return @temp1
+        end
+    end
+    def in_celsius
+        if @hash.has_key?(:c)
+            return @hash[:c] 
+        elsif @hash.has_key?(:f)
+            return (@hash[:f]-32)*5.to_f/9  #formula to convert from F to C
+        elsif @temp2.is_a? Numeric
+            return (@temp2-32)*5.to_f/9
+        else
+            return @temp1
+        end
+    end
+end
